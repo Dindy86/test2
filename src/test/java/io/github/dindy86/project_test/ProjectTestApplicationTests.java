ad7a368a96d01d878db5d8080fb892a8af714842
Dindy Pierre <ing.pierredindy@gmail.com>
2025-05-28 17:44:19 -0300

    First commit

---
+++ src/test/java/io/github/dindy86/project_test/ProjectTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dindy86.project_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
